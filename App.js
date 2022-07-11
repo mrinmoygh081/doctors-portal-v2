6b51ffd36fc772bfa05aac1ba5cc0975a4d09a55
Mrinmoy Ghosh <mrinmoygh081@gmail.com>
2022-07-11 19:18:48 +0530

    mediosoft first commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+// const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+import Splash from "./screens/Splash";
+import Dashboard from "./screens/Dashboard";
+import Profile from "./screens/ProfileScreen";
+import Explore from "./screens/ExploreScreen";
+import MainTabScreen from "./screens/MainTabScreen";
+import RootStackScreen from "./screens/RootStackScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <RootStackScreen />
+        {/* <Drawer.Navigator initialRouteName="Splash">
+          <Drawer.Screen name="Splash" component={MainTabScreen} />
+        </Drawer.Navigator> */}
+      </NavigationContainer>
+      <StatusBar style="light" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{
+  /* <Stack.Navigator
+          screenOptions={{
+            title: "Splash Screen Screen",
+            headerStyle: {
+              backgroundColor: "#009387",
+            },
+            headerTintColor: "#fff",
+            // headerShown: false,
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Splash"
+            component={Splash}
+            options={{
+              title: "Splash Screen Screen",
+              headerStyle: {
+                backgroundColor: "#009387",
+              },
+            }}
+          />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator> */
+}
